2d70d9d172d86bda285b44a9cd9a3e0f39d6bed0
Starinin Andrey <starininandrey@gmail.com>
2021-01-18 17:58:02 +0300

    Инициализация проекта

---
+++ IT_STEP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IT_STEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
