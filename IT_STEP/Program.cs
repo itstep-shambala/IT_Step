ef98ca51b249685a60c259ea823ad3877f7d9fa2
anst-foto <starininandrey@gmail.com>
2021-01-25 17:25:23 +0300

    Добавление функций по вводу данных студента и вывода данных всех студентов

--- IT_STEP/Program.cs
+++ IT_STEP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace IT_STEP
@@ -43,27 +44,88 @@ namespace IT_STEP
             }
         }
 
+        static void AddStudentToList(ref List<Student> students)
+        {
+            string select;
+            string firstName;
+            string lastName;
+            DateTime dateOfBirth;
+            string faculty;
+            int numberOfGroup;
+            double averageMark;
+            Student tempStudent;
+            
+            do
+            {
+                Console.Write("Введите имя: ");
+                firstName = Console.ReadLine();
+                Console.Write("Введите фамилию: ");
+                lastName = Console.ReadLine();
+                Console.WriteLine("Введите дату рождения: ");
+                dateOfBirth = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Введите факультет: ");
+                faculty = Console.ReadLine();
+                Console.Write("Введите номер группы: ");
+                numberOfGroup = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите среднюю оценку: ");
+                averageMark = Convert.ToDouble(Console.ReadLine());
+
+                tempStudent = new Student(firstName, lastName, dateOfBirth, faculty, numberOfGroup, averageMark);
+                students.Add(tempStudent);
+
+                Console.Write("Нажмите Y - если хотите ввести данные ещё одного студента");
+                select = Console.ReadLine();
+            } while (select == "Y" || select == "y");
+        }
+
+        static void ShowStudent(Student student)
+        {
+            Console.WriteLine($"{student.FirstName} {student.LastName}, {student.DateOfBirth:d}, {student.Faculty} {student.NumberOfGroup}, {student.AverageMark}");
+        }
+        static void ShowStudents(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                ShowStudent(student);
+            }
+        }
+
         static void Main()
         {
             var student = new Student("Andrey", "Starinin", DateTime.Now, "SD", 1, 2);
-            ShowMenu();
-            var choice = Convert.ToChar(Console.ReadLine());
-            switch (choice)
+            var students = new List<Student>();
+            students.Add(student);
+
+            string select;
+
+            do
             {
-                case '1':
-                    break;
-                case '2':
-                    break;
-                case '3':
-                    break;
-                case '4':
-                    break;
-                case '5':
-                    Console.Write("Введите имя файла: ");
-                    var path = Console.ReadLine();
-                    ExportToFile(student, path);
-                    break;
-            }
+                ShowMenu();
+                var choice = Convert.ToChar(Console.ReadLine());
+                switch (choice)
+                {
+                    case '1':
+                        ShowStudents(students);
+                        break;
+                    case '2':
+                        AddStudentToList(ref students);
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        Console.Write("Введите имя файла: ");
+                        var path = Console.ReadLine();
+                        ExportToFile(student, path);
+                        break;
+                }
+                
+                Console.Write("Нажмите Y - если хотите продолжить");
+                select = Console.ReadLine();
+            } while (select == "Y" || select == "y");
+            
+            Console.WriteLine("До свидания...");
         }
     }
 }
